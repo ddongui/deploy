b3e075bb74e0f635e4a0638229dc38a1377ad8fa
SunRui <2932626714@qq.com>
2022-08-26 14:40:57 +0800

    #

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
     clean: true,
     dts: "src/index.ts",
     // 删除bin中的cjs和入口中的mjs
-    onSuccess: "del dist\\bin\\index.js dist\\index.cjs "
+    onSuccess: "del dist\\bin\\index.js dist\\index.cjs dist\\chunk-*.js"
 })
